021b1b8dd7618882b7f91511066806937d322fe9
lavish08-jain <lavishj08@gmail.com>
2026-07-11 13:22:26 +0530

    Create form.php

---
+++ day-7/form.php
@@ -0,0 +1,11 @@
+<?php
+$count= 1;
+$count++;
+
+echo'count value is: $count';
+?>
+
+<form method ="get">
+    <input type="text" name="username">
+    <button type ="submit"> send</button>
+</form>
